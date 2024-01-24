ecdbdf0a5559bb8a9e384717ede4aa3e853b40d5
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-01-24 10:28:25 -0600

    Refactor TodoController to use TodoRepository

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,39 +1,32 @@
 import { Response, Request } from 'express';
 import { prisma } from '../../data/postgres';
 import { CreateTodoDto, UpdateTodoDto } from '../../domain/dtos';
+import { TodoRepository } from '../../domain';
 
 export class TodoController {
   // DI: Dependency injection
-  constructor() {}
+  constructor(private readonly todoRepository: TodoRepository) {}
 
   public getTodos = async (req: Request, res: Response) => {
-    const todos = await prisma.todo.findMany({});
+    const todos = await this.todoRepository.getAll();
     return res.json(todos);
   };
 
   public getTodoById = async (req: Request, res: Response) => {
     const id = +req.params.id;
-    const todo = await prisma.todo.findFirst({ where: { id } });
-    if (!todo)
-      return res
-        .status(400)
-        .json({ error: `No se ha encontrado un todo con el id ${id}` });
-
-    return res.json(todo);
+    try {
+      const todo = await this.todoRepository.findById(id);
+      return res.json(todo);
+    } catch (error) {
+      res.status(400).json({ error });
+    }
   };
 
   public createTodo = async (req: Request, res: Response) => {
     const [error, createTodoDto] = CreateTodoDto.create(req.body);
     if (error) return res.status(400).json({ error });
-    const { text } = req.body;
-    if (!text)
-      return res.status(400).json({ error: 'text property is required' });
 
-    // usamos el signo "!" porque ya tenemos "createTodoDto"
-    const todo = await prisma.todo.create({
-      data: createTodoDto!,
-    });
-
+    const todo = await this.todoRepository.create(createTodoDto!);
     return res.json(todo);
   };
 
@@ -45,32 +38,13 @@ export class TodoController {
     });
 
     if (error) return res.status(400).json({ error });
-
-    const todo = await prisma.todo.findFirst({ where: { id } });
-    if (!todo) {
-      return res.status(400).json({
-        error: `No se ha encontrado un todo con el id ${id}`,
-      });
-    }
-    const updatedTodo = await prisma.todo.update({
-      where: { id },
-      data: updateTodoDto!.values,
-    });
-
+    const updatedTodo = await this.todoRepository.updateById(updateTodoDto!);
     return res.json(updatedTodo);
   };
 
   public deleteTodo = async (req: Request, res: Response) => {
     const id = +req.params.id;
-
-    const todo = await prisma.todo.findFirst({ where: { id } });
-    if (!todo) {
-      return res.status(400).json({
-        error: `No se ha encontrado un todo con el id ${id}`,
-      });
-    }
-    await prisma.todo.delete({ where: { id } });
-
-    return res.status(201).json({});
+    const deletedTodo = await this.todoRepository.deleteById(id);
+    return res.json(deletedTodo);
   };
 }
