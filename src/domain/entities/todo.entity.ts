fa7e56f030367c33ef81b9ade8af78cf326ceffb
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-01-24 09:00:47 -0600

    Add static method to convert object to TodoEntity

--- src/domain/entities/todo.entity.ts
+++ src/domain/entities/todo.entity.ts
@@ -8,4 +8,21 @@ export class TodoEntity {
   get isCompleted() {
     return !!this.completedAt;
   }
+
+  public static fromObject(object: { [key: string]: any }): TodoEntity {
+    const { id, text, completedAt } = object;
+
+    if (!id) throw 'id is required';
+    if (!text) throw 'text is required';
+
+    let newCompletedAt;
+    if (newCompletedAt) {
+      newCompletedAt = new Date(completedAt);
+      if (isNaN(newCompletedAt.getTime())) {
+        throw 'CompletedAt is not a valid date';
+      }
+    }
+
+    return new TodoEntity(id, text, completedAt);
+  }
 }
