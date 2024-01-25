bb7b738d55c0d4f2a273b727a26108c64cdc5fc6
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-01-25 14:05:08 -0600

    Add tests for retrieving TODOs from API

--- tests/presentation/todos/routes.test.ts
+++ tests/presentation/todos/routes.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { testServer } from '../../test-server';
+import { prisma } from '../../../src/data/postgres';
 
 describe('Todo route testing', () => {
   // Iniciamos el servidor antes de todas las pruebas
@@ -14,9 +15,23 @@ describe('Todo route testing', () => {
     testServer.close();
   });
 
+  const todo1 = { text: 'Buy coffe' };
+  const todo2 = { text: 'Buy bread' };
+
   test('should return TODOs api/todos', async () => {
-    const response = await request(testServer.app)
+    await prisma.todo.deleteMany();
+    await prisma.todo.createMany({
+      data: [todo1, todo2],
+    });
+
+    const { body } = await request(testServer.app)
       .get('/api/todos')
       .expect(200);
+
+    expect(body).toBeInstanceOf(Array);
+    expect(body.length).toBe(2);
+    expect(body[0].text).toBe(todo1.text);
+    expect(body[1].text).toBe(todo2.text);
+    expect(body[0].comppletedAy).toBeNull();
   });
 });
