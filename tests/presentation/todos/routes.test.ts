ab7d73a7ce61347032fad8f7f88ccdf56a67f9e9
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-01-25 12:02:07 -0600

    Add dotenv-cli package and update test scripts (prisma en Testing)

--- tests/presentation/todos/routes.test.ts
+++ tests/presentation/todos/routes.test.ts
@@ -2,12 +2,21 @@ import request from 'supertest';
 import { testServer } from '../../test-server';
 
 describe('Todo route testing', () => {
-  // Iniciamos el servidor
+  // Iniciamos el servidor antes de todas las pruebas
   beforeAll(async () => {
     await testServer.start();
   });
 
+  // Despues de todas las pruebas
+  afterAll(() => {
+    // Quita el warning (el servidor no se
+    //  terminó de manera agraciada)
+    testServer.close();
+  });
+
   test('should return TODOs api/todos', async () => {
-    const response = await request(testServer.app).get('/api/todos');
+    const response = await request(testServer.app)
+      .get('/api/todos')
+      .expect(200);
   });
 });
